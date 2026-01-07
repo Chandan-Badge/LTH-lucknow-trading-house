13bf11efcf85550333a33a3e4a26184a0b9b3e85
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2026-01-07 14:17:46 +0530

    Adding infinite scroll

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -34,6 +34,94 @@ function HeroSection() {
                 </Link>
 
                 {/* </div> */}
+
+                <div className="stock-ticker">
+                    <ul>
+                        <li className="minus">
+                            <span className="company">AAPL</span>
+                            <span className="price">181.16</span>
+                            <span className="change">-1.36 (-0.75%)</span>
+                        </li>
+                        <li className="plus">
+                            <span className="company">TSLA</span>
+                            <span className="price">199.40</span>
+                            <span className="change">+7.43 (+3.87%)</span>
+                        </li>
+                        <li className="plus">
+                            <span className="company">NFLX</span>
+                            <span className="price">587.65</span>
+                            <span className="change">+4.09 (+0.70%)</span>
+                        </li>
+                        <li className="minus">
+                            <span className="company">GOOG</span>
+                            <span className="price">138.75</span>
+                            <span className="change">-6.54 (-4.50%)</span>
+                        </li>
+                        <li className="plus">
+                            <span className="company">NVDA</span>
+                            <span className="price">790.92</span>
+                            <span className="change">+2.75 (+0.35%)</span>
+                        </li>
+                        <li className="minus">
+                            <span className="company">MSFT</span>
+                            <span className="price">407.54</span>
+                            <span className="change">-2.80 (-0.68%)</span>
+                        </li>
+                        <li className="plus">
+                            <span className="company">META</span>
+                            <span className="price">487.05</span>
+                            <span className="change">+5.31 (+1.10%)</span>
+                        </li>
+                        <li className="minus">
+                            <span className="company">KO</span>
+                            <span className="price">60.34</span>
+                            <span className="change">-0.37 (-0.61%)</span>
+                        </li>
+                    </ul>
+
+                    <ul aria-hidden="true">
+                        <li className="minus">
+                            <span className="company">AAPL</span>
+                            <span className="price">181.16</span>
+                            <span className="change">-1.36 (-0.75%)</span>
+                        </li>
+                        <li className="plus">
+                            <span className="company">TSLA</span>
+                            <span className="price">199.40</span>
+                            <span className="change">+7.43 (+3.87%)</span>
+                        </li>
+                        <li className="plus">
+                            <span className="company">NFLX</span>
+                            <span className="price">587.65</span>
+                            <span className="change">+4.09 (+0.70%)</span>
+                        </li>
+                        <li className="minus">
+                            <span className="company">GOOG</span>
+                            <span className="price">138.75</span>
+                            <span className="change">-6.54 (-4.50%)</span>
+                        </li>
+                        <li className="plus">
+                            <span className="company">NVDA</span>
+                            <span className="price">790.92</span>
+                            <span className="change">+2.75 (+0.35%)</span>
+                        </li>
+                        <li className="minus">
+                            <span className="company">MSFT</span>
+                            <span className="price">407.54</span>
+                            <span className="change">-2.80 (-0.68%)</span>
+                        </li>
+                        <li className="plus">
+                            <span className="company">META</span>
+                            <span className="price">487.05</span>
+                            <span className="change">+5.31 (+1.10%)</span>
+                        </li>
+                        <li className="minus">
+                            <span className="company">KO</span>
+                            <span className="price">60.34</span>
+                            <span className="change">-0.37 (-0.61%)</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
